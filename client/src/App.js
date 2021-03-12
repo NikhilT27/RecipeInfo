5265f38e305972bca902b791cc372f169c3fa678
NikhilT27 <nikhilthakare14@gmail.com>
2021-03-12 15:26:02 +0530

    add: few changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [token, setToken] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      setToken(true);
+    } else {
+      setToken(false);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">{token ? <Home /> : <Main />}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
